2401f9fe9309e39dd8e14373545616a8a901f349
etgfiles <ezekielgiordano@gmail.com>
2020-06-30 19:32:44 -0400

    router back in

--- src/react/App.js
+++ src/react/App.js
@@ -3,16 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import GamesContainer from './GamesContainer'
 import KowOuterContainer from './kow/containers/KowOuterContainer'
 
+// The routing is done here in the full version
 const App = props => {
 	return (
-		<div>
-			<BrowserRouter>
-				<Switch>
-					<Route path='/' exact component={GamesContainer} />
-					<Route path='/kow' exact component={KowOuterContainer} />
-				</Switch>
-			</BrowserRouter>
-		</div>
+		<BrowserRouter>
+			<Switch>
+				<Route path='/' exact component={GamesContainer} />
+				<Route path='/kow' exact component={KowOuterContainer} />				
+			</Switch>
+		</BrowserRouter>
 	)
 }
 
