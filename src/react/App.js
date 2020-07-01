e13c1bf865ab95d8cdb903e735f4f012e68d111e
etgfiles <ezekielgiordano@gmail.com>
2020-07-01 14:27:52 -0400

    Trying HashRouter

--- src/react/App.js
+++ src/react/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import { Router, Route } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
+import { HashRouter, Route } from 'react-router-dom'
 import GamesContainer from './GamesContainer'
 import KowOuterContainer from './kow/containers/KowOuterContainer'
 
-let history = createBrowserHistory()
-
 const App = props => {
 	return (
-		<Router history={history}>
+		<HashRouter>
 			<Route path='/' exact component={GamesContainer} />
 			<Route path='/kow' exact component={KowOuterContainer} />				
-		</Router>
+		</HashRouter>
 	)
 }
 
