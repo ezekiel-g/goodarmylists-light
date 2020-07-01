053df722fd16122803c9ffeda4f964099f4800e2
etgfiles <ezekielgiordano@gmail.com>
2020-07-01 13:36:36 -0400

    Router instead of BrowserRouter

--- src/react/App.js
+++ src/react/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import { Router, Route, Switch } from 'react-router-dom'
 import GamesContainer from './GamesContainer'
 import KowOuterContainer from './kow/containers/KowOuterContainer'
 
-// The routing is done here in the full version
+let history = createBrowserHistory()
+
 const App = props => {
 	return (
-		<BrowserRouter>
+		<Router history={history}>
 			<Switch>
 				<Route path='/' exact component={GamesContainer} />
 				<Route path='/kow' exact component={KowOuterContainer} />				
 			</Switch>
-		</BrowserRouter>
+		</Router>
 	)
 }
 
